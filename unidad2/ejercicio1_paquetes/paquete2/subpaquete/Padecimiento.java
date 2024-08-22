14376a717d48139a9b5163ee59b19905f7d5eb34
ingvega <isc.p.vega@gmail.com>
2024-08-22 16:10:02 -0600

    Delete unidad2/ejercicio1_paquetes directory

--- unidad2/ejercicio1_paquetes/paquete2/subpaquete/Padecimiento.java
+++
@@ -1,9 +0,0 @@
-package unidad2.ejercicio1_paquetes.paquete2.subpaquete;
-
-import paquete1.Paciente;
-
-public class Padecimiento {
-    private String nombre;
-    
-    private Paciente[] pacientes; 
-}
