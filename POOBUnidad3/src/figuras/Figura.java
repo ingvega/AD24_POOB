ef05348a27afba2041267bd688f10225d6897ca2
ingvega <isc.p.vega@gmail.com>
2024-10-02 16:50:02 -0600

    Integración parcial de figuras con herencia

---
+++ POOBUnidad3/src/figuras/Figura.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package figuras;
+import java.awt.Color;
+
+/**
+ *
+ * @author paveg
+ */
+public class Figura {
+    private Color color;
+    private Punto puntoReferencia;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Punto getPuntoReferencia() {
+        return puntoReferencia;
+    }
+
+    public void setPuntoReferencia(Punto puntoReferencia) {
+        this.puntoReferencia = puntoReferencia;
+    }
+    
+//    public double calcularArea(){
+//    
+//    }
+    public void desplazar(int ajusteX, int ajusteY){
+        int x=puntoReferencia.getX();
+        int y=puntoReferencia.getY();
+        puntoReferencia.setX(x+ajusteX);
+        puntoReferencia.setY(y+ajusteY);
+    }
+}
